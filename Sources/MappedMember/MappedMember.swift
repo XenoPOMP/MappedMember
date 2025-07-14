d302ebf36ed5f7fa01e1336dfbfff79f016a4a8d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-07-14 10:24:09 +0200

    🪲 fix naming

--- Sources/MappedMember/MappedMember.swift
+++ Sources/MappedMember/MappedMember.swift
@@ -1,5 +1,5 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-@attached(peer)
+@attached(peer, names: arbitrary)
 public macro Mapped(_ title: String) = #externalMacro(module: "MappedMemberMacros", type: "Mapped")
