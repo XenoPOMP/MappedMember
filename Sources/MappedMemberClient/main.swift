03ca07e867a0ac2e671cedf5f51b6349ed71536e
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-07-14 10:45:47 +0200

    📝 change main

--- Sources/MappedMemberClient/main.swift
+++ Sources/MappedMemberClient/main.swift
@@ -1,5 +1,16 @@
 import MappedMember
+import Foundation
 
-struct Test: Codable {
-    @Mapped("AGENCY_1") var agency: Int
+struct Test {
+    @Mapped("AGENCY_1") var agency: Int = 12
+    
+    init(AGENCY_1: Int) {
+        self.AGENCY_1 = AGENCY_1
+    }
 }
+
+var test = Test(AGENCY_1: 12)
+print("get test.agency = \(test.agency)")
+
+test.agency = 13
+print("get test.agency = \(test.agency)")
